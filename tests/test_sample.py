bcee85e7925bf003e0026822466695ae6270d0e4
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2023-01-24 00:12:41 +0530

    edge cases for generate (cropping + greedy determinism)

---
+++ tests/test_sample.py
@@ -0,0 +1,39 @@
+"""tests for the generate() method."""
+
+import torch
+import pytest
+
+from src.model import GPT, GPTConfig
+
+
+@pytest.fixture
+def tiny_model():
+    cfg = GPTConfig(vocab_size=20, block_size=8, n_layer=1, n_head=2, n_embd=16)
+    return GPT(cfg), cfg
+
+
+def test_generate_extends_sequence(tiny_model):
+    model, cfg = tiny_model
+    idx = torch.tensor([[1, 2, 3]], dtype=torch.long)
+    out = model.generate(idx, max_new_tokens=5)
+    assert out.shape == (1, 8)
+
+
+def test_generate_does_not_exceed_block_size(tiny_model):
+    """when prompt is longer than block_size, model should crop and still work."""
+    model, cfg = tiny_model
+    long_prompt = torch.randint(0, cfg.vocab_size, (1, cfg.block_size + 4))
+    out = model.generate(long_prompt, max_new_tokens=3)
+    assert out.shape[1] == cfg.block_size + 4 + 3
+
+
+def test_generate_top_k(tiny_model):
+    """with top_k=1 (greedy), output should be deterministic across runs."""
+    model, _ = tiny_model
+    model.eval()
+    idx = torch.tensor([[1, 2, 3]], dtype=torch.long)
+    torch.manual_seed(0)
+    o1 = model.generate(idx, max_new_tokens=5, temperature=1.0, top_k=1)
+    torch.manual_seed(99)  # different seed should not matter for greedy
+    o2 = model.generate(idx, max_new_tokens=5, temperature=1.0, top_k=1)
+    assert torch.equal(o1, o2)
